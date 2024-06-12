74c5aa6d89b9d72780d96e19d2b6c943bb0337b5
ReMueff <veronika.anuf39@gmail.com>
2024-06-12 15:39:35 +0300

    add base elements

---
+++ app/src/main/java/com/example/diploma/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.diploma
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, MainStartFragment())
+                .commit()
+        }
+    }
+}
---
+++ app/user_interface/src/main/java/com/example/user_interface/MainUserActivity.kt
@@ -0,0 +1,50 @@
+package com.example.user_interface
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.user_interface.databinding.ActivityMainUserBinding
+
+class MainUserActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainUserBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaseFragment(UserCalendar())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.ic_calendar -> replaseFragment(UserCalendar())
+                R.id.ic_chat -> replaseFragment(UserChat())
+                R.id.ic_profile -> replaseFragment(UserProfile())
+
+                else -> {
+
+
+                }
+
+
+            }
+            true
+
+        }
+
+        }
+
+    private fun replaseFragment (fragment: Fragment){
+            val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout_rep, fragment)
+        fragmentTransaction.commit()
+
+
+
+
+    }
+}
